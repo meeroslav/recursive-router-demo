72879c71f3edd42275105d3d6e155d9b7384f395
Miroslav Jonas <missing.manual@gmail.com>
2020-07-23 13:10:58 +0200

    chore: add tap to monitor passed params

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { FullUser, User } from '../model/user';
+import { FullUser } from '../model/user';
 import { GithubService } from '../service/github.service';
 import { RouteParams } from 'angular-routing';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user',
@@ -13,10 +13,14 @@ import { switchMap } from 'rxjs/operators';
 export class UserComponent implements OnInit {
   user$: Observable<FullUser>;
 
-  constructor(private readonly service: GithubService, private routeParams$: RouteParams<{ username: string }>) { }
+  constructor(
+    private readonly service: GithubService,
+    private readonly routeParams$: RouteParams<any>
+  ) { }
 
   ngOnInit(): void {
     this.user$ = this.routeParams$.pipe(
+      tap(params => console.log('FOUND PARAMS', params)),
       switchMap(({ username }) => this.service.getUser(username))
     );
   }
